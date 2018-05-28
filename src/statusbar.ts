5f3a2fa5cdb38ffe91d136413a859bdb99d1243f
Fabio Spampinato <spampinabio@gmail.com>
2018-05-28 22:15:02 +0200

    Removed some unused code

--- src/statusbar.ts
+++ src/statusbar.ts
@@ -5,7 +5,6 @@ import * as pify from 'pify';
 import * as request from 'request';
 import * as vscode from 'vscode';
 import Config from './config';
-import Utils from './utils';
 
 /* STATUSBAR */
 
